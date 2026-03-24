e284ae64ccebbb0526b495dce445564603338b5b
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2026-03-24 20:41:43 +0300

    -

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,6 +7,6 @@ return {
     keys = {
         { "<leader>pf", "<cmd>Telescope find_files<cr>", desc = "Lists files in your current working directory, respects .gitignore" },
         { "<C-p>", "<cmd>Telescope git_files<cr>", desc = "Fuzzy search through the output of git ls-files command, respects .gitignore" },
-        { "<leader>ps", function() require("telescope.builtin").live_grep() end, desc = "Searches for the string under your cursor or selection in your current working directory" }
+        { "<leader>pg", function() require("telescope.builtin").live_grep() end, desc = "Searches for the string under your cursor or selection in your current working directory" }
     }
 }
